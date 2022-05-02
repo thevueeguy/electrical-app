bbe43ceddb2f5bd6ff39cb0b308de32a057af0b9
Sharad Kushwah <sharadrkushwah@gmail.com>
2022-05-02 15:30:06 +0530

    error resolved

--- src/Components-Nav/ContactMe.jsx
+++ src/Components-Nav/ContactMe.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import ScriptTag from 'react-script-tag';
+import React, { useEffect } from 'react'
 
 
 const ContactMe = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+      
+        script.src = "https://platform.linkedin.com/badges/js/profile.js";
+        script.async = true;
+      
+        document.body.appendChild(script);
+      
+        return () => {
+          document.body.removeChild(script);
+        }
+      }, []);
+
   return (
     <div>
-      <ScriptTag isHydrating={true} type="text/javascript" 
-      src="https://platform.linkedin.com/badges/js/profile.js" />
-      <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="sharad-kushwah" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/sharad-kushwah?trk=profile-badge">Sharad Kushwah</a></div>
+      <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="sharad-kushwah" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/sharad-kushwah?trk=profile-badge">Sharad Kushwah</a></div>
     </div>
   )
 }
